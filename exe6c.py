c97d6624f578e1d86a6fd5d80e9f5c5af6992b6b
frederico volkmann <fredericovolkmann19@gmail.com>
2025-03-04 15:52:57 -0300

    Add files via upload

---
+++ exe6c.py
@@ -0,0 +1,5 @@
+def somar(a, b):
+    return a + b
+
+print(somar(5, 3))  
+print(somar("Olá, ", "mundo!"))  
---
+++ exex6b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int int= 20;
+    float flut = 4.5;
+    double resultado;
+
+    resultado = int + flut; 
+
+    printf("Resultado: %lf\n", resultado); 
+
+    return 0;
+}
